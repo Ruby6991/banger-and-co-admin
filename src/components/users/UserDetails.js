1e1bec49a02b30bf66f80f7d8f7a0910ff53bfd1
rabiya faizal <rabiyaf1@gmail.com>
2020-06-08 06:08:43 +0530

    update customer state functionality

--- src/components/users/UserDetails.js
+++ src/components/users/UserDetails.js
@@ -18,12 +18,34 @@ class UserDetails extends Component{
             viewDocs:false
         }
         this.viewDocs = this.viewDocs.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
     }
 
     viewDocs(){
         this.setState({
             viewDocs:true
         })
+    }
+
+    handleUpdate(){
+        const that = this;
+        const config = {
+            headers:{
+                Authorization:'Bearer '+ localStorage.token
+            }
+        }
+        const returningUser = {
+            customerState:'Returning'
+        }
+            axios.put("http://localhost:8080/UpdateUserState/"+ that.state.email, returningUser, config)
+            .then(function(res){
+                console.log("User Marked as Returning");
+                alert("User Marked as Returning");
+                window.location.reload();
+            }).catch(function(error){
+                console.log("User state update un-successful!\nError : ",error.response);
+                alert("User state update  un-successful!");
+        }) 
     }
 
     render(){
@@ -47,6 +69,11 @@ class UserDetails extends Component{
                 <td>{this.state.address}</td>
                 <td class="teal lighten-4">{this.state.phoneNo}</td>
                 <td class="center">{this.state.customerState} <br/> 
+                {this.state.customerState==="Returning"?(
+                    <button class="waves-effect waves-light btn-small red lighten-2 disabled" type="button" onClick={this.handleUpdate}>Mark as Returning</button>
+                ):(
+                    <button class="waves-effect waves-light btn-small red lighten-2" type="button" onClick={this.handleUpdate}>Mark as Returning</button>
+                )}
                 </td>
                 <td class="teal lighten-4"><button class="waves-effect btn-flat grey darken-2 white-text" onClick={this.viewDocs}>Docs</button></td>
             </tr>
